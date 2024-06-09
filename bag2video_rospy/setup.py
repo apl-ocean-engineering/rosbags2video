574eb962d2669891908f995c5f8814adf56c266a
Aaron Marburg <amarburg@uw.edu>
2024-06-09 08:41:48 -0700

    Convert rospy version to use bag2video_common

--- bag2video_rospy/setup.py
+++ bag2video_rospy/setup.py
@@ -9,4 +9,6 @@ setuptools.setup(
     version="0.0.1",
     python_requires=">=3.6",
     install_requires=["numpy", "opencv-python", "imageio[ffmpeg]", "rospy"],
+    packages=["bag2video_rospy", "bag2video_common"],
+    package_dir={"bag2video_common": "../bag2video_common", "bag2video": "."},
 )
